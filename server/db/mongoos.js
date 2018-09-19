3d884f68cad7bcc41be4c650e7b9cf25e3909f89
mohamed <mgbara081@gmail.com>
2018-09-19 16:33:05 +0200

    initial to deploy

--- server/db/mongoos.js
+++ server/db/mongoos.js
@@ -1,9 +1,12 @@
 const mongoos = require("mongoose");
 const dbName = "todo";
 const localUrl = "mongodb://localhost/"+dbName;
+const user = "mohamed";
+const password = "m010095707099";
+const cloudUrl = `mongodb://${user}:${password}@ds261332.mlab.com:61332/${dbName}`;
   mongoos.Promise = global.Promise;
   mongoos.set('useFindAndModify', false);
-  mongoos.connect(localUrl,{ useNewUrlParser: true });
+  mongoos.connect(cloudUrl,{ useNewUrlParser: true });
 
 
 
