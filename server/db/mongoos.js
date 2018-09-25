c004fe2aa276b8853f1e4a74f5b77b7558f2b1dc
mohamed <mgbara081@gmail.com>
2018-09-26 01:53:34 +0200

    add private routeds

--- server/db/mongoos.js
+++ server/db/mongoos.js
@@ -3,14 +3,14 @@ const dbName = "todos";
 const localUrl = "mongodb://localhost/"+dbName;
 const user = "mohamed";
 const password = "m010095707099";
-const cloudUrl = `mongodb://${user}:${password}@ds261332.mlab.com:61332/${dbName}`;
+const cloudUrl = `mongodb://${user}:${password}@ds215093.mlab.com:15093/${dbName}`;
 
 
   mongoos.Promise = global.Promise;
   mongoos.set('useFindAndModify', false);
   mongoos.set('useCreateIndex',true );
 
-  mongoos.connect(localUrl,{ useNewUrlParser: true});
+  mongoos.connect(cloudUrl,{ useNewUrlParser: true});
 
 
 
